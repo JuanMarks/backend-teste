e5f06f073505efc40b5574608392fc907233a3c3
JuanMarks <juandev77@gmail.com>
2025-07-14 19:11:47 -0300

    atualizando

--- src/place/places.module.ts
+++ src/place/places.module.ts
@@ -4,9 +4,14 @@ import { PlacesController } from './places.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { MulterModule } from '@nestjs/platform-express';
+// 2. Importe o memoryStorage
+import { memoryStorage } from 'multer';
 
 @Module({
-  imports: [PrismaModule, CloudinaryModule],
+  imports: [PrismaModule, CloudinaryModule, MulterModule.register({
+      storage: memoryStorage(), 
+    }),],
   controllers: [PlacesController],
   providers: [PlacesService],
   exports: [PlacesService],
